e69ce73b7e0150175ba09e0f5d28213f9e02fd68
Dragos Tudorache <hedphug@gmail.com>
2013-03-15 00:23:29 +0200

    Update MatchesTableViews.js

--- js/app/views/MatchesTableViews.js
+++ js/app/views/MatchesTableViews.js
@@ -14,10 +14,19 @@ var RowItem = Backbone.View.extend({
 
 var Container = component.ui.TableView.extend({
   _rowSelector: 'li.TheMatch',
-
+  
+  // This render should be made from the adapter
+  // as a hackish method to wrap the already generated html
+  // with a view object
   wrapRenderedContent: function() {
     var adapter = this.getAdapter();
     var view = null;
+    
+    // The view doesn't need to know about the adapter
+    // The iteration can be made from the adapter and for each item
+    // a view caa be constructed
+    // [adapter.eachVisualItem / adapter.eachPrerenderedItem]
+    // could be used to iterate over each adapter without needing to query the dom!!!
     this.eachRow(function(elem) {
       view = adapter.buidViewFromElement(elem);
       adapter.addViewItem(elem.attr('data-matchid'), view);
