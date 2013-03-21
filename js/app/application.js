cdfa6f9c361b6a7993c478f75044bb7aaf000f7d
Dragos Tudorache <hedphug@gmail.com>
2013-03-21 16:25:31 +0200

    Added rtf module

--- js/app/application.js
+++ js/app/application.js
@@ -2,16 +2,42 @@
 // Application
 
 define(['app/components',
+  'skm/rtf/RTFApi',
   'controllers/MatchesTableViewController'],
-  function(component, MatchesTableViewController)
+  function(component, RTF, MatchesTableViewController)
 {
 'use strict';
 
 
+var tplhtml = com.betbrain.nextLiveMatches.matchesList(jsonMatches);
+$('#NextLiveMatchesRTF').html(tplhtml);
+
+
 var matchesTableController = MatchesTableViewController.create();
+matchesTableController.processMatchesInitialDump(jsonMatches);
+window.mtc = matchesTableController;
 
 
-window.matchesTableController = matchesTableController;
+
+return;
+
+RTF.Config.setWSUrl('ws://radu.betbrain.com:8080/rtfws');
+RTF.Config.setXHRUrl('http://radu.betbrain.com/rtfajax');
+
+var rtf;
+window.RTFApi = rtf = RTF.Api.get();
+
+rtf.setClientId((new Date).getTime());
+rtf.addSubscription('nextLiveMatches');
+rtf.startUpdates();
+
+rtf.getSubscription('nextLiveMatches').on('update', function(update) {
+  var updatesArray = update['message'];
+  _.each(updatesArray, function(matchesUpdatesItem) {
+    matchesTableController.processMatchesListUpdates(matchesUpdatesItem);
+  });
+  
+});
 
 
 });
