d453ce99307408d81d86f548f888b74d6c83e17d
Dragos Tudorache <hedphug@gmail.com>
2013-03-21 16:27:02 +0200

    Removed unnecessary fields

--- js/app/models/OutcomeCellModel.js
+++ js/app/models/OutcomeCellModel.js
@@ -1,17 +1,7 @@
 define([], function(){
 	'use strict';
 
-	var OutcomeCellModel = Backbone.Model.extend( {
-		/**
-		 * Model must be initialized with this attribute as a JSON object
-		 * @type {[JSON]}
-		 */
-		match: null,
-
-    initialize: function() {
-      cl('%cnew OutcomeCellModel', 'color:#A2A2A2');
-    }
-	});
+	var OutcomeCellModel = Backbone.Model.extend();
 
 	return OutcomeCellModel; 
 });
