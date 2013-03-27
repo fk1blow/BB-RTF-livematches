c84621b08758fb41b4365b9148ea27dc4983bac2
Dragos Tudorache <hedphug@gmail.com>
2013-03-27 17:14:54 +0200

    Modified the row insertion method - can insert at index
    Now, if an index is specified, a new row can be inserted there instead
    of the tail of the table - defaults to tail/appending at the end.

--- js/app/views/MatchesTable/WrapperView.js
+++ js/app/views/MatchesTable/WrapperView.js
@@ -20,9 +20,27 @@ var Wrapper = component.ui.TableView.extend({
     this._rowViews = {};
   },
 
-  renderRow: function(view) {
+  /**
+   * Inserts a new row view
+   *
+   * @description Inserts a new element to the table view,
+   * at the tail or at a specified index. If the index is not found or not
+   * speficied, it will be appended at the end of the table view.
+   * @param  {Object} view  a reference to an instance of EventItemView
+   * @param  {Number} index the index at which the view's el should pe placed
+   */
+  renderRow: function(view, index) {
     console.log('%cWrapper.renderRow', 'color:green', view.$el);
-    this.$el.append(view.$el);
+    var $elAtIndex = null, $view = view.$el;
+    if ( index ) {
+      $elAtIndex = this.el.find('li.TheMatch').eq(index);
+      if ( $elAtIndex.length )
+        $view.insertBefore($elAtIndex);
+      else
+        this.$el.append($view);
+    } else {
+      this.$el.append($view);
+    }
   },
 
   getNewRow: function(options) {
